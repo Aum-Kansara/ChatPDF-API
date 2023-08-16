3cd4bc5fc3408482721073b93846fdde752ec438
Aum Kansara <aumkan23@gmail.com>
2023-08-16 10:48:54 +0530

    updated

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-python --version
+apt update && sudo apt upgrade
+apt install python3.11
+ls -l /usr/bin/python*
+python3.11 --version
 python3 -m pip install --upgrade pip
 pip install --disable-pip-version-check -r requirements.txt
