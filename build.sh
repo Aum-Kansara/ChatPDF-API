36e69541a38f99f94e2f7e055b1e8dc8ef6ab40d
Aum Kansara <aumkan23@gmail.com>
2023-08-16 10:52:06 +0530

    updated

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-apt update && sudo apt upgrade
-apt install python3.11
-ls -l /usr/bin/python*
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.11
+ls -l /usr/bin/python3*
 python3.11 --version
 python3 -m pip install --upgrade pip
 pip install --disable-pip-version-check -r requirements.txt
